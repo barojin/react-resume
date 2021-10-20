36cf6cd694710a802bba63a943a7ca63ef47ff40
barojin <baro9959@gmail.com>
2021-10-20 13:29:05 -0700

    contract integrated with emailjs

--- src/Components/contactForm.js
+++ src/Components/contactForm.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { Fade, Slide } from "react-reveal";
 
 export default function ContactUs() {
   const form = useRef();
@@ -17,16 +18,104 @@ export default function ContactUs() {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" placeholder="Your name"/>
-      <label>Email</label>
-      <input type="email" name="email" placeholder="Your Email Address"/>
-      <label>Subject</label>
-      <input type="text" name="email" placeholder="Subject"/>
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <section id="contact">
+      <Fade bottom duration={1000}>
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
+
+          <div className="ten columns">
+            <p className="lead">To Hojin Nam</p>
+          </div>
+        </div>
+      </Fade>
+
+      <div className="row">
+        <Slide left duration={1000}>
+          <div className="eight columns">
+            <form ref={form} onSubmit={sendEmail} id="contactForm" name="contactForm">
+              <fieldset>
+                <div>
+                  <label htmlFor="contactName">
+                    Name <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactName"
+                    name="name"
+                    placeholder="Your name"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactEmail">
+                    Email <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactEmail"
+                    name="email"
+                    placeholder="Your Email Address"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactSubject">Subject</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactSubject"
+                    name="subject"
+                    placeholder="Subject"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactMessage">
+                    Message <span className="required">*</span>
+                  </label>
+                  <textarea
+                    cols="50"
+                    rows="15"
+                    id="contactMessage"
+                    name="message"
+                  ></textarea>
+                </div>
+
+                <div>
+                <input type="submit" value="Send" />
+                  <span id="image-loader">
+                    <img alt="" src="images/loader.gif" />
+                  </span>
+                </div>
+              </fieldset>
+            </form>
+
+            <div id="message-warning"> Error boy</div>
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
+            </div>
+          </div>
+        </Slide>
+
+        <Slide right duration={1000}>
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <img src="images/blackgoldwhite.png" alt="Logo" width="10%"/>
+            </div>
+
+          </aside>
+        </Slide>
+      </div>
+    </section>
   );
 };
